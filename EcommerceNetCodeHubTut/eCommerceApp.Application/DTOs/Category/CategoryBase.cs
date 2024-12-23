5ed311184864227c8228284d739d476b8828c255
shalant <doug.rosenberg@gmail.com>
2024-12-23 09:11:00 -0600

    initial setup and created domain models plus DTOs

---
+++ EcommerceNetCodeHubTut/eCommerceApp.Application/DTOs/Category/CategoryBase.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceApp.Application.DTOs.Category
+{
+    public class CategoryBase
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ EcommerceNetCodeHubTut/eCommerceApp.Application/DTOs/Product/UpdateProduct.cs
@@ -0,0 +1,7 @@
+﻿namespace eCommerceApp.Application.DTOs.Product
+{
+    public class UpdateProduct : ProductBase
+    {
+        public Guid Id { get; set; }
+    }
+}
